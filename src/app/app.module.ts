34ff3f1efacbd50c607595c56452b5232fd518d2
acalvet <calvet.alexandre@epitech.eu>
2022-07-08 01:36:26 +0200

    [ADD] component to display maze

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PlayerComponent } from './player/player.component';
 import { ParametersComponent } from './parameters/parameters.component';
+import { DisplayMazeComponent } from './display-maze/display-maze.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PlayerComponent,
-    ParametersComponent
+    ParametersComponent,
+    DisplayMazeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-maze/display-maze.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-maze',
+  templateUrl: './display-maze.component.html',
+  styleUrls: ['./display-maze.component.scss']
+})
+export class DisplayMazeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
